2bb0656231c4e476233ab249e1a104ef714d2b11
Eryk Fryderyk <grzeniu098@gmail.com>
2020-11-21 13:39:32 +0100

    add background component

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,28 @@
 <template>
+  <Background />
   <div class="container">
     <h1>Hello World</h1>
   </div>
 </template>
 
 <script>
+import HeroBackground from './components/HeroBackground.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      title: 'Hello Grzegorz',
+    };
+  },
   components: {
+    Background: HeroBackground,
   },
 };
 </script>
 
-<style lang="scss">
+<style
+    HeroBackground lang="scss">
 *{
   padding: 0;
   margin: 0;
@@ -22,11 +31,12 @@ export default {
 
 body{
   font-size: 17px;
-  background-color: black;
+  // background-color: black;
   color: white;
 }
 .container{
   width: 100%;
+  height: 100vh;
   margin: 0 auto 0 auto;
   padding: 0 15px 0 15px;
 }
---
+++ src/components/HeroBackground.vue
@@ -0,0 +1,21 @@
+<template>
+<div class="">
+  <div class="hero-background"/>
+</div>
+</template>
+
+<script>
+export default {
+  name: 'HeroBackground',
+};
+</script>
+
+<style lang="scss" scoped>
+.hero-background{
+  width: 100%;
+  height: 100vh;
+  position: fixed;
+  background-color: red;
+  z-index: -1;
+}
+</style>
