5b0b608e64fc32abcca0cd471ddfe4070e7d03b8
Eryk Fryderyk <grzeniu098@gmail.com>
2020-11-26 22:46:46 +0100

    update component, add new icons

--- src/components/Expenses.vue
+++ src/components/Expenses.vue
@@ -10,7 +10,9 @@
         345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
         </svg>
       </div>
+      <img src="../assets/img/house.svg" alt="">
       <h3>Dom</h3>
+      <span>200,00zł</span>
     </div>
     <div class="box">
       <div class="box-icon">
@@ -22,7 +24,9 @@
         345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
         </svg>
       </div>
+      <img src="../assets/img/invoice.svg" alt="">
       <h3>Rachunki</h3>
+      <span>200,00zł</span>
     </div>
     <div class="box">
       <div class="box-icon">
@@ -34,7 +38,9 @@
         345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
         </svg>
       </div>
+      <img src="../assets/img/house.svg" alt="">
       <h3>Samochód</h3>
+      <span>200,00zł</span>
     </div>
     <div class="box">
       <div class="box-icon">
@@ -46,7 +52,51 @@
         345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
         </svg>
       </div>
+      <img src="../assets/img/salad.svg" alt="">
       <h3>Jedzenie</h3>
+      <span>200,00zł</span>
+    </div>
+    <div class="box">
+      <div class="box-icon">
+        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
+        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
+        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
+        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
+        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
+        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
+        </svg>
+      </div>
+      <img src="../assets/img/star.svg" alt="">
+      <h3>Zachcianki</h3>
+      <span>200,00zł</span>
+    </div>
+    <div class="box">
+      <div class="box-icon">
+        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
+        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
+        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
+        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
+        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
+        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
+        </svg>
+      </div>
+      <img src="../assets/img/life.svg" alt="">
+      <h3>Zdrowie</h3>
+      <span>200,00zł</span>
+    </div>
+    <div class="box">
+      <div class="box-icon">
+        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
+        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
+        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
+        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
+        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
+        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
+        </svg>
+      </div>
+      <img src="../assets/img/save-money.svg" alt="">
+      <h3>Oszczędności</h3>
+      <span>200,00zł</span>
     </div>
   </div>
 </template>
@@ -75,5 +125,9 @@ export default {
   justify-content: center;
   align-items: center;
   transform: rotate(-90deg);
+}
+img{
+  width: 30px;
+  height: 30px;
 }
 </style>
