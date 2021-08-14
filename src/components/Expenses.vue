541bd59ab9beec54fc738d118cd9f5a64d13736c
Eryk Fryderyk <grzeniu098@gmail.com>
2021-08-14 17:11:05 +0200

    fix function which show and hide elements from category

--- src/components/Expenses.vue
+++ src/components/Expenses.vue
@@ -34,157 +34,6 @@
         </li>
       </ul>
     </div>
-    <!-- <div class="expenses-bar expenses-bar--close">
-      <div class="box-icon">
-        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="" xml:space="preserve">
-        <path  d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
-        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
-        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
-        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
-        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
-        </svg>
-      </div>
-      <img class="expenses-bar__icon" src="../assets/img/wallet.svg" alt="">
-      <div class="expenses-bar__item">
-        <h3 class="expenses-bar__title">Rachunki</h3>
-        <span class="expenses-bar__price">200,00zł</span>
-      </div>
-    </div>
-    <div class="expenses-box expenses-box--close">
-      <ul class="expenses-box__list">
-        <li class="expenses-box__item">
-          Prąd<span class="expenses-box__price">200zł</span>
-        </li>
-        <li class="expenses-box__item">Telewizja<span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Internet<span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Telefon<span class="expenses-box__price">200zł</span></li>
-      </ul>
-    </div>
-    <div class="expenses-bar expenses-bar--close">
-      <div class="box-icon">
-        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
-        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
-        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
-        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
-        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
-        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
-        </svg>
-      </div>
-      <img class="expenses-bar__icon" src="../assets/img/riding-car.svg" alt="">
-      <div class="expenses-bar__item">
-        <h3 class="expenses-bar__title">Samochód</h3>
-        <span class="expenses-bar__price">200,00zł</span>
-      </div>
-    </div>
-    <div class="expenses-box expenses-box--close">
-      <ul class="expenses-box__list">
-        <li class="expenses-box__item">Paliwo<span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Ubezpieczenie
-          <span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Przegląd<span class="expenses-box__price">200zł</span></li>
-      </ul>
-    </div>
-    <div class="expenses-bar expenses-bar--close">
-      <div class="box-icon">
-        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
-        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
-        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
-        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
-        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
-        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
-        </svg>
-      </div>
-      <img class="expenses-bar__icon" src="../assets/img/salad.svg" alt="">
-      <div class="expenses-bar__item">
-        <h3 class="expenses-bar__title">Jedzenie</h3>
-        <span class="expenses-bar__price">200,00zł</span>
-      </div>
-    </div>
-    <div class="expenses-box expenses-box--close">
-      <ul class="expenses-box__list">
-        <li class="expenses-box__item">Sklep Lidl<span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Biedronka<span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Pizzeria<span class="expenses-box__price">200zł</span></li>
-      </ul>
-    </div>
-    <div class="expenses-bar expenses-bar--close">
-      <div class="box-icon">
-        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
-        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
-        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
-        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
-        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
-        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
-        </svg>
-      </div>
-      <img class="expenses-bar__icon" src="../assets/img/star.svg" alt="">
-      <div class="expenses-bar__item">
-        <h3 class="expenses-bar__title">Zachcianki</h3>
-        <span class="expenses-bar__price">200,00zł</span>
-      </div>
-    </div>
-    <div class="expenses-box expenses-box--close">
-      <ul class="expenses-box__list">
-        <li class="expenses-box__item">Bluzka Cropp
-          <span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Buty Martens
-          <span class="expenses-box__price">200zł</span></li>
-        <li class="expenses-box__item">Zegarek Casio
-          <span class="expenses-box__price">200zł</span></li>
-      </ul>
-    </div>
-    <div class="expenses-bar expenses-bar--close">
-      <div class="box-icon">
-        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
-        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
-        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
-        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
-        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
-        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
-        </svg>
-      </div>
-      <img class="expenses-bar__icon" src="../assets/img/life.svg" alt="">
-      <div class="expenses-bar__item">
-        <h3 class="expenses-bar__title">Zdrowie</h3>
-        <span class="expenses-bar__price">200,00zł</span>
-      </div>
-    </div>
-    <div class="expenses-box expenses-box--close">
-      <ul class="expenses-box__list">
-        <li class="expenses-box__item">
-          Wizyta u Lekarza<span class="expenses-box__price">200zł</span>
-        </li>
-        <li class="expenses-box__item">
-          Lekarstwa<span class="expenses-box__price">200zł</span>
-        </li>
-      </ul>
-    </div>
-    <div class="expenses-bar expenses-bar--close">
-      <div class="box-icon box-icon--active">
-        <svg class="box-icon__arrow" version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" width="451.847px" height="451.847px" viewBox="0 0 451.847 451.847" style="enable-background:new 0 0 451.847 451.847;" xml:space="preserve">
-        <path d="M225.923,354.706c-8.098,0-16.195-3.092-22.369-9.263L9.27,
-        151.157c-12.359-12.359-12.359-32.397,0-44.751 c12.354-12.354,
-        32.388-12.354,44.748,0l171.905,171.915l171.906-171.909c12.359-12.354,
-        32.391-12.354,44.744,0 c12.365,12.354,12.365,32.392,0,44.751L248.292,
-        345.449C242.115,351.621,234.018,354.706,225.923,354.706z"/>
-        </svg>
-      </div>
-      <img class="expenses-bar__icon" src="../assets/img/save-money.svg" alt="">
-      <div class="expenses-bar__item">
-        <h3 class="expenses-bar__title">Oszczędności</h3>
-        <span class="expenses-bar__price">200,00zł</span>
-      </div>
-    </div>
-    <div class="expenses-box expenses-box--close">
-      <ul class="expenses-box__list">
-        <li class="expenses-box__item">
-          Wakacje<span class="expenses-box__price">200zł</span>
-        </li>
-        <li class="expenses-box__item">
-          Nowy rower<span class="expenses-box__price">200zł</span>
-        </li>
-      </ul>
-    </div> -->
   </div>
 </template>
 
@@ -192,87 +41,42 @@
 export default {
   name: 'Expenses',
   props: [
-    'categoryName',
+    'items',
   ],
   data() {
     return {
       soldo: null,
-      items: [
-        {
-          icon: '/img/home.ab898512.svg',
-          title: 'Mieszkanie',
-          totalPrice: 10,
-          hide: true,
-          id: 1,
-          elems: [
-            {
-              id: Math.random(),
-              name: 'Pierogi',
-              price: 200,
-            },
-          ],
-        },
-        // {
-        //   icon: '/img/home.ab898512.svg',
-        //   title: 'Dziewczyna',
-        //   totalPrice: 10,
-        //   hide: true,
-        //   id: 2,
-        //   elems: [
-        //     {
-        //       id: Math.random(),
-        //       name: 'Saszłyk',
-        //       price: 12,
-        //     },
-        //     {
-        //       id: Math.random(),
-        //       name: 'Kupa',
-        //       price: 1,
-        //     },
-        //   ],
-        // },
-        // {
-        //   icon: '/img/home.ab898512.svg',
-        //   title: 'Dziewczyna',
-        //   totalPrice: 10,
-        //   hide: true,
-        //   id: 3,
-        //   elems: [
-        //     {
-        //       id: Math.random(),
-        //       name: 'Kopytk',
-        //       price: 123,
-        //     },
-        //   ],
-        // },
-      ],
     };
   },
   watch: {
-    categoryName(val, oldVal) {
-      if (val !== oldVal) {
-        this.items.push({
-          id: Math.random(),
-          title: val,
-          icon: '/img/home.ab898512.svg',
-          totalPrice: 0,
-          hide: true,
-          elems: [
-            // {
-            //   id: Math.random(), name: 'dodany', price: 10,
-            // },
-          ],
-        });
-      }
-    },
+    // categoryName(val, oldVal) {
+    //   if (val !== oldVal) {
+    //     this.items.push({
+    //       id: Math.random(),
+    //       title: val,
+    //       icon: '/img/home.ab898512.svg',
+    //       totalPrice: 0,
+    //       hide: true,
+    //       elems: [
+    //         // {
+    //         //   id: Math.random(), name: 'dodany', price: 10,
+    //         // },
+    //       ],
+    //     });
+    //   }
+    // },
   },
   methods: {
     clickEvent(id) {
-      const index = this.items.findIndex((el) => el.id === id);
+      const index = this.items.findIndex((el) => id === el.id);
+      console.log(index);
+      console.log(this.items[1]);
+      // eslint-disable-next-line vue/no-mutating-props
       this.items[index].hide = !this.items[index].hide;
     },
     createNewExpensesBar() {
       console.log(this.categoryName);
+      // eslint-disable-next-line vue/no-mutating-props
       this.items.push({
         id: Math.random(),
         name: this.categoryName,
